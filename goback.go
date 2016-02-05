0d95c65f9ac55d9090f193b27a043f21661890bd
Marin Atanasov Nikolov <dnaeon@gmail.com>
2016-02-05 11:43:03 +0200

    Style formatting

--- goback.go
+++ goback.go
@@ -1,11 +1,15 @@
 // Package goback implements a simple exponential backoff
 //
-// An exponential backoff approach is typically used when treating with potentially
-// faulty/slow systems. If a system fails quick retries may exacerbate the system
-// specially when the system is dealing with several clients. In this case a backoff
-// provides the faulty system enough room to recover.
+// An exponential backoff approach is typically used when
+// treating with potentially faulty/slow systems. If a system
+// fails quick retries may exacerbate the system especially when the
+// system is dealing with several clients.
+//
+// In this case a backoff provides the faulty system enough room
+// for recovery.
 //
 // Simple example:
+//
 //  func main() {
 //	    b := &goback.SimpleBackoff(
 //		    Min:    100 * time.Millisecond,
@@ -21,6 +25,7 @@
 //  }
 //
 // More examples can be found in the examples folder in the repository.
+
 package goback
 
 import (
@@ -31,8 +36,9 @@ import (
 )
 
 var (
-	// ErrMaxAttemptsExceeded indicates that the maximum retries has been
-	// excedeed. Usually to consider a service unreachable/unavailable.
+	// ErrMaxAttemptsExceeded indicates that the maximum number of
+	// retries has excedeed.
+	// Usually to consider a service unreachable/unavailable.
 	ErrMaxAttemptsExceeded = errors.New("maximum of attempts exceeded")
 )
 
@@ -40,8 +46,9 @@ var (
 type Backoff interface {
 	// NextAttempt returns the duration to wait for the next retry.
 	NextAttempt() (time.Duration, error)
-	// Reset clears the number of tries. Next call to NextAttempt will return
-	// the minimum backoff time (if there is no error).
+
+	// Reset clears the number of tries. Next call to NextAttempt will
+	// return the minimum backoff time (if there is no error).
 	Reset()
 }
 
@@ -59,19 +66,23 @@ func (b *SimpleBackoff) NextAttempt() (time.Duration, error) {
 	if b.MaxAttempts > 0 && b.Attempts >= b.MaxAttempts {
 		return 0, ErrMaxAttemptsExceeded
 	}
+
 	next := GetNextDuration(b.Min, b.Max, b.Factor, b.Attempts)
 	b.Attempts++
+
 	return next, nil
 }
 
-// Reset clears the number of tries. Next call to NextAttempt will return
-// the minimum backoff time (if there is no error).
+// Reset clears the number of attempts.
+// Further calls to NextAttempt will return the
+// minimum backoff time (if there is no error).
 func (b *SimpleBackoff) Reset() {
 	b.Attempts = 0
 }
 
-// JitterBackoff provides an strategy similar to SimpleBackoff but lightly randomises
-// the duration to minimise collisions between contending clients.
+// JitterBackoff provides a strategy similar to SimpleBackoff, but
+// lightly randomises the duration to minimise collisions between
+// contending clients.
 type JitterBackoff SimpleBackoff
 
 // NextAttempt returns the duration to wait for the next retry.
@@ -79,45 +90,57 @@ func (b *JitterBackoff) NextAttempt() (time.Duration, error) {
 	if b.MaxAttempts > 0 && b.Attempts >= b.MaxAttempts {
 		return 0, ErrMaxAttemptsExceeded
 	}
+
 	next := GetNextDuration(b.Min, b.Max, b.Factor, b.Attempts)
 	next = addJitter(next, b.Min)
 	b.Attempts++
+
 	return next, nil
 }
 
-// GetNextDuration returns the duration for the strategies considering the minimum and
-// maximum durations, the factor of increase and the number of attemtps tried.
+// GetNextDuration returns the duration for the strategies
+// considering the minimum and maximum durations, the factor, and the
+// number of attempts already tried.
 func GetNextDuration(min, max time.Duration, factor float64, attempts int) time.Duration {
 	d := time.Duration(float64(min) * math.Pow(factor, float64(attempts)))
+
 	if d > max {
 		return max
 	}
+
 	return d
 }
 
-// Wait sleeps for the duration of the time specified by the backoff strategy.
+// Wait sleeps for the duration of the time specified by the
+// backoff strategy.
 func Wait(b Backoff) error {
 	next, err := b.NextAttempt()
+
 	if err != nil {
 		return err
 	}
+
 	time.Sleep(next)
+
 	return nil
 }
 
-// After returns a channel that will be called after the time specified by the backoff
-// strategy or will exit immediately with an error.
+// After returns a channel that will be called after the time
+// specified by the backoff strategy or will exit immediately with an error.
 func After(b Backoff) <-chan error {
 	c := make(chan error, 1)
 	next, err := b.NextAttempt()
+
 	if err != nil {
 		c <- err
 		return c
 	}
+
 	go func() {
 		time.Sleep(next)
 		c <- nil
 	}()
+
 	return c
 }
 
